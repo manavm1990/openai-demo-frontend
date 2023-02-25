55bb398484dd6dd4ed75419a4634a0479ba24840
Manav Misra <manavm@visionwebsoft.com>
2023-02-25 16:28:04 -0600

    feat: 💫 pulse animate on load

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import initBg from "./bg";
 import renderers from "./renderers.js";
 import initSelectListener from "./selectListener";
 
+const bg = document.querySelector("#bg");
 const errorP = document.querySelector("p");
 const ol = document.querySelector("ol");
 
@@ -9,12 +10,17 @@ initBg();
 initSelectListener((val) => {
   renderers.renderError(errorP, "");
 
+  bg.classList.add("animate-pulse");
+
   fetch("http://localhost:3000/val")
     .then((res) => res.json())
     .then((data) => {
+      console.log("done fetching");
       renderers.renderList(ol, data.completion.split(", "));
     })
     .catch((err) => {
       renderers.renderError(errorP, err);
+    }).finally(() => {
+      bg.classList.remove("animate-pulse");
     });
 });
