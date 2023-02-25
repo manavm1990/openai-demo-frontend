959a9fcf6e680c7dad616e986c495544c45cc301
Manav Misra <manavm@visionwebsoft.com>
2023-02-25 08:05:56 -0600

    feat: :construction: WIP log names from server

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import initSelectListener from './selectListener';
 
 initBg();
 initSelectListener((val) => {
-  console.log(val);
+  fetch("http://localhost:3000/val").then((res) => {
+    // TODO: handle response by putting it in the DOM
+    console.log("res", res);
+  });
 });
